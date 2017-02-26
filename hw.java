cd6617bd319288b70fb074e3b4187034c956f36b
Alvaro Tzul <tzulalvaro@gmail.com>
2017-02-25 20:27:00 -0800

    status report

--- hw.java
+++ hw.java
@@ -3,7 +3,7 @@ public class hw
     public static void main(String[] args)
     {
         System.out.println("Hello Bitch");
-		System.out.println("2nd line "); // to to push this line of code
-		out.println("This is modified through commands") //
+		System.out.println("2nd line "); // to to push this line of code  status: failed to push through command 
+		out.println("This is modified through commands") //  status: granted, push was a success. edit this on github.com
     }
 }
