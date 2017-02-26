e3489f902675275d787625e8e8c6ceb921763f2c
Thomas Mejia <tommym147@gmail.com>
2017-02-25 22:42:24 -0800

    Added and removed some print outs

--- hw.java
+++ hw.java
@@ -3,9 +3,10 @@ public class hw
     public static void main(String[] args)
     {
         System.out.println("Jingle Bells");
-		System.out.println("Jingle Bells "); 
-		System.out.println("Jingle all the way.")
+		System.out.println("Jingle Bells ");
 		
 		System.out.println("This is a test");
+		
+		System.out.println("Let it snow");
     }
 }
