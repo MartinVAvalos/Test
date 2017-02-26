3ddff80656521a4628be487c13f176abb63408fe
Thomas Mejia <tommym147@gmail.com>
2017-02-25 23:15:48 -0800

    left lyrics

--- hw.java
+++ hw.java
@@ -2,11 +2,9 @@ public class hw
 {
     public static void main(String[] args)
     {
-		
+		System.out.println("Jingle Bells");
+		System.out.println("Jingle Bells");
 		System.out.println("This is a test");
 		
-		System.out.println("Let it snow");
-		System.out.println("Let it snow");
-		System.out.println("Let it snow");
     }
 }
