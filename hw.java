6fa52d3671d8bd17f8214250e2319dfedc45c448
Thomas Mejia <tommym147@gmail.com>
2017-02-25 23:07:32 -0800

    just let lyrics of song

--- hw.java
+++ hw.java
@@ -2,11 +2,10 @@ public class hw
 {
     public static void main(String[] args)
     {
-        System.out.println("Jingle Bells");
-		System.out.println("Jingle Bells ");
 		
 		System.out.println("This is a test");
 		
+		System.out.println("Let it snow");
 		System.out.println("Let it snow");
 		System.out.println("Let it snow");
     }
